82124fc24dcd56aa246ed382c952f25300181e3f
Richard Brosler <richard.brosler@sp.senai.br>
2025-08-15 15:27:28 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,53 @@
+# javascript-tools
+Este repositório tem a finalidade de apoiar os alunos do curso de Desenvolvimento em Javascript e sites utilizados no curso.
+# Aplicativo de Apoio
+### Download do aplicativo de Apoio
+No endereço abaixo encontra-se um aplicativo de apoio para os repositórios git:  
+https://senai701.brosler.pro.br/release/SetupApoioJavaScript.exe
+# Sites importantes:
+### Github
+https://github.com/
+### Repositório do projeto do professor
+https://github.com/richard-brosler-senai/m25-javascript-02
+### Download programa Git
+https://git-scm.com/downloads
+# Comandos importantes do GIT
+### Configurar usuário local
+```bash
+git config --global user.name "Seu nome"
+```
+### Configurar e-mail do usuário local
+```bash
+git config --global user.email "seu.email@dominio.com.br"
+```
+### Clonar um repositório
+Na pasta (C:\Users\Aluno\Documents\) efetuar o comando abaixo:
+```bash
+git clone <endereço do repositório.git> m25-javascript-02
+```
+### Baixar alterações do Servidor Git para o local
+Na pasta do Repositório (C:\Users\Aluno\Documents\m25-javascript-02) efetuar o comando abaixo:
+```bash
+git pull
+```
+### Adicionar itens ao versionamento
+Na pasta do Repositório (C:\Users\Aluno\Documents\m25-javascript-02) efetuar o comando abaixo:
+```bash
+git add .
+```
+### Fechar pacote de versionamento
+Na pasta do Repositório (C:\Users\Aluno\Documents\m25-javascript-02) efetuar o comando abaixo:
+```bash
+git commit -m "Colocar um comentário sobre a versão"
+```
+### Subir alterações do local para o Servidor Git
+Na pasta do Repositório (C:\Users\Aluno\Documents\m25-javascript-02) efetuar o comando abaixo:
+```bash
+git push
+```
+# Sequência diária da nossa aula:
+* Configurar o <a href="#configurar-usuário-local">`usuário local do git`</a> e o <a href="#configurar-e-mail-do-usuário-local">`email do usuário local do git`</a> **no início da aula**
+* Efetuar o <a href="#clonar-um-repositório">`clone do seu repositório`</a> para a pasta C:\Users\Aluno\Documents\m25-javascript-02 **no início da aula**
+* Trabalhar os conteúdos dentro da pasta **C:\Users\Aluno\Documents\m25-javascript-02**
+* **Antes do término da aula**, ou **ao término de cada lição** <a href="#adicionar-itens-ao-versionamento">`Adicionar itens ao versionamento`</a>, <a href="#fechar-pacote-de-versionamento">`Fechar o pacote de versionamento`</a> e <a href="#subir-alterações-do-local-para-o-servidor-git">`Subir alterações do local para o Servidor Git`</a>
+
---
+++ python_code/apoio.py
@@ -0,0 +1,67 @@
+import questionary, re, requests, subprocess, os
+from click import clear
+from urllib.request import urlopen
+from dotenv import load_dotenv
+from platform import node
+load_dotenv(override=True)
+"""
+Programa para apoiar os alunos no curso de Java para clonar seu repositório
+"""
+def validar_email_robusto(texto):
+    padrao = r'^[\w\.-]+@[\w\.-]+\.\w{2,}$'
+    return re.match(padrao, texto) is not None
+    
+def menu():
+    chave = os.environ.get('CHAVEAPP')
+    senha = os.environ.get('SENHAAPP')
+    turma = "m25-javascript-02"
+    dirgit= f"{os.environ.get('APPDATA')}\\..\\Local\\Programs\\Git"
+    if not os.path.exists(dirgit):
+        dirgit = f"{os.environ.get('ProgramFiles')}\\git"
+        if not os.path.exists(dirgit):
+            dirgit = "D:\\Program Files\\git"
+   
+    # Verificando se o diretório existe
+    diretorio = f"{os.environ.get('USERPROFILE')}\\Documents\\{turma}"
+    confirmar = os.path.isdir(diretorio)
+    # Se não existir, clonando o repositório
+    while not confirmar:
+        clear()
+        email = questionary.text("Qual é o seu E-mail?", validate=lambda text: True if validar_email_robusto(text) else "E-mail está em formato inválido!.").ask()
+        nome = repositorio = ""
+        url = f"https://senai701.brosler.pro.br/ws-patrimonio/api/diario-fic-email/{chave}?email={email}"
+        resposta = requests.get(url, auth=('Senai', senha))
+        if resposta.status_code == 200:
+            nome = resposta.json().get('diario')['nome']
+            repositorio = resposta.json().get('diario')['repositorio']
+        nome = questionary.text("Informar seu nome: ",default=nome).ask()
+        repositorio = questionary.text("Informar seu repositório do git: ",default=repositorio).ask()
+        confirmar = questionary.confirm("As informações estão corretas?", default=False).ask()
+        if confirmar:
+            url = f"https://senai701.brosler.pro.br/ws-patrimonio/api/diario-fic/{chave}"
+            ret = requests.post(url, json={
+                "nome": nome,
+                "repositorio": repositorio,
+                "email": email,
+                "maquina": node()
+            }, headers={"Content-Type": "application/json"})
+            # print(ret.json())
+            # Configurando o git
+            subprocess.run([f"{dirgit}\\bin\\git.exe", "config", "--global", "user.name", f'"{nome}"'])
+            subprocess.run([f"{dirgit}\\bin\\git.exe", "config", "--global", "user.email", f'"{email}"'])
+            subprocess.run([f"{dirgit}\\bin\\git.exe", "config", "--global", "core.editor", '"code --wait"'])
+            subprocess.run([f"{dirgit}\\bin\\git.exe", "config", "--global", "merge.tool", "vscode"])
+            subprocess.run([f"{dirgit}\\bin\\git.exe", "config", "--global", "mergetool.vscode.cmd", '"code --wait --merge \$REMOTE \$BASE \$LOCAL \$MERGED"'])
+            subprocess.run([f"{dirgit}\\bin\\git.exe", "config", "--global", "mergetool.keepBackup", "false"])
+            # Clonando o repositório
+            subprocess.run([f"{dirgit}\\bin\\git.exe", "clone", repositorio, diretorio])
+    # Verificando se o arquivo subir.bat existe
+    if not os.path.exists(diretorio + "\\subir.bat"):
+        with urlopen("https://raw.githubusercontent.com/richard-brosler-senai/javascript-tools/refs/heads/master/subir.bat") as response:
+            conteudo = response.read().decode('utf-8')
+            conteudo = conteudo.replace("\r\n", "\n")
+            conteudo = conteudo.replace("\n", "\r\n")
+            with open(diretorio + "\\subir.bat", "w", newline='',encoding='utf-8') as file:
+                file.write(conteudo)
+if __name__ == "__main__":
+    menu()
